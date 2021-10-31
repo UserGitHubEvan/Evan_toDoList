6ca0301bf23fbb66eb3c59e871fa71c92cd4309a
UserGitHubEvan <doccccccc384@gmail.com>
2021-11-01 01:35:34 +0200

    Logic to navigate from the Registration screen to the TaskListViewController screen.

--- toDoList/RegistrationViewController.swift
+++ toDoList/RegistrationViewController.swift
@@ -12,8 +12,8 @@ class RegistrationViewController: BaseViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
-//    let expectedEmail: String = "test@test.com"
-//    let expectedPassword: String = "123456789"
+    let expectedEmail: String = "test@test.com"
+    let expectedPassword: String = "123456789"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,36 +22,36 @@ class RegistrationViewController: BaseViewController {
     }
     
     @IBAction func registrationAction(_ sender: Any) {
-//        print("registrationAction")
+        print("registrationAction")
     }
     
-//    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-//        if let email = emailTextField.text,
-//           let password = passwordTextField.text {
-//            
-//            if email.contains("@") {
-//                if password.count > 8 {
-//                    if email == expectedEmail {
-//                        if password == expectedPassword {
-//                            print("Success")
-//                            return true
-//                        } else {
-//                            print("Fail: wrong password")
-//                        }
-//                    } else {
-//                        print("Fail: wrong email")
-//                    }
-//                    
-//                } else {
-//                    print("Fail: password must be at leats 8 symbols")
-//                }
-//            } else {
-//                print("Fail: wrong email format")
-//            }
-//        } else {
-//            print("Fail: empty fields")
-//        }
-//        return false
-//    }
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if let email = emailTextField.text,
+           let password = passwordTextField.text {
+            
+            if email.contains("@") {
+                if password.count > 8 {
+                    if email == expectedEmail {
+                        if password == expectedPassword {
+                            print("Success")
+                            return true
+                        } else {
+                            print("Fail: wrong password")
+                        }
+                    } else {
+                        print("Fail: wrong email")
+                    }
+                    
+                } else {
+                    print("Fail: password must be at leats 8 symbols")
+                }
+            } else {
+                print("Fail: wrong email format")
+            }
+        } else {
+            print("Fail: empty fields")
+        }
+        return false
+    }
 
 }
